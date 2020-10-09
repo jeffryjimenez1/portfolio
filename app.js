c4ff5fadeab8c9749cd06e4dd4c7f4e463901e77
Jeffry Jimenez <codingfulltime@gmail.com>
2020-10-08 22:26:46 -0600

    added animations

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const myCarousel = document.querySelector(".carousel");
 const hamburgerBtn = document.querySelector(".navMenu-btn");
 const mainNav = document.querySelector(".main-nav");
 const navLink = document.querySelectorAll(".nav-item");
+const titlePortfolio = document.querySelector(".titlePortfolio");
+const subtitleSite = document.querySelector(".subtitleSite");
+const mainPic = document.querySelector(".showcase-pic");
+
 
 let slideIndex = 0;
 showSlides();
 
+
+
+// create slideshow
 function showSlides() {
     let i;
     let carouselSlides = document.getElementsByClassName("carousel-item");
@@ -34,7 +41,7 @@ navLink.forEach( function(link){
     })
 })
 
-
-
-
-// create slideshow
+gsap.from(titlePortfolio,{duration: 2, y: '-400%', opacity: 0});
+gsap.from(subtitleSite,{duration: 2, x: '-500%', delay: 1});
+gsap.from(mainNav,{duration: 2, x: '-100%', opacity: 0, delay: 1.2});
+gsap.from(mainPic,{duration: 2, x: '100%', delay: 1.3});
