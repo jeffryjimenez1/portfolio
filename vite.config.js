e34031d55b65df54df294517222b6b6c736abaae
Jeffry Jimenez <shino17jt@gmail.com>
2025-02-02 22:27:44 -0600

    changed vite again

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
+import { fileURLToPath, URL } from 'node:url'
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
 export default defineConfig({
-  base: './', // Ensures relative asset paths
   plugins: [
     vue(),
     vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      vue: 'vue/dist/vue.esm-bundler.js', // Make sure to resolve the Vue module properly
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-});
+})
