3bd6c6ed8b89c2ce0931fb5821722d45bf58d3ec
Jeffry Jimenez <shino17jt@gmail.com>
2025-02-02 18:19:40 -0600

    fixed deploying

--- portfolioWebsite/vite.config.js
+++ portfolioWebsite/vite.config.js
@@ -15,4 +15,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    outDir: 'portfolioWebsite',  // Specify the output directory, no redundant "portfolio"
+  }
 })
